ee27afe473d94db5e9ea60677d4691c0acabf3b3
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-11-20 14:41:09 -0300

    feat: add difficulty levels

--- jogo_adivinhacao.cpp
+++ jogo_adivinhacao.cpp
@@ -1,37 +1,75 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "*********************************" <<  endl;
-    cout << "* Welcome to the Guessing Game! *" <<  endl;
-    cout << "*********************************" <<  endl;
+int main()
+{
 
     const int SECRET_NUMBER = 42;
     bool guessed = false;
     int attempts = 0;
+    int dificulty_guess_number;
 
     double points = 1000.0;
 
+    cout << "*********************************" << endl;
+    cout << "* Welcome to the Guessing Game! *" << endl;
+    cout << "*********************************" << endl;
 
-    while (!guessed){
+    cout << "Choose the level of dificulty";
+    cout << "Easy (E), Medium (M), Hard (H): ";
+
+    char dificulty;
+    cin >> dificulty;
+
+    
+
+    if (dificulty == 'E')
+    {
+        dificulty_guess_number = 15;
+    }
+    else if (dificulty == 'M')
+    {
+        dificulty_guess_number = 10;
+    }
+    else
+    {
+        dificulty_guess_number = 5;
+    }
+
+    for (attempts = 1; attempts <= dificulty_guess_number; attempts++)
+    {
         cout << "What's your guess? ";
         int guess_number;
         cin >> guess_number;
-        
+
         guessed = guess_number == SECRET_NUMBER;
 
-        double missing_points = abs((guess_number - SECRET_NUMBER) / 2);
+        double missing_points = abs(guess_number - SECRET_NUMBER) / 2.0;
         points = points - missing_points;
-    
-        if (guessed) {
+
+        if (guessed)
+        {
             cout << "Congratulations! You guessed the secret number!" << endl;
-        } else if (guess_number > SECRET_NUMBER) {
+        }
+        else if (guess_number > SECRET_NUMBER)
+        {
             cout << "Your guess was highest than the secret number" << endl;
-        } else {
+        }
+        else
+        {
             cout << "You guess was lowest than the secret number" << endl;
         }
-        attempts++;
     }
-    cout << "Game over. Your finished the game with: " << attempts << "attempts" << endl;
-    cout << "Your score was: " << points << " points" << endl;
+
+    if (guessed == false)
+    {
+        cout << "You lose. Try again!" << endl;
+    }
+    else
+    {
+        cout << "Game over. Your finished the game with: " << attempts << "attempts" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Your score was: " << points << " points" << endl;
+    }
 }
